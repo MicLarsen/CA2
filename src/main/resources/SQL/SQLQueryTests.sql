48405c54911d49913ebfb102376bd660a34126a9
Michael <Michael@10.50.138.70>
2016-10-04 11:46:38 +0200

    initial

--- src/main/resources/SQL/SQLQueryTests.sql
+++ src/main/resources/SQL/SQLQueryTests.sql
@@ -1,21 +1,17 @@
-/**
- * Author:  Michael
- * Created: 03-10-2016
- */
-
--- SELECT * FROM `address`; 
--- SELECT * FROM `cityinfo`;
--- SELECT * FROM `compoany`;
--- SELECT * FROM `hobby`;
--- SELECT * FROM `infoentity`;
--- SELECT * FROM `person`;
--- SELECT * FROM `person_hobby`;
--- SELECT * FROM `phone`;
+SELECT * FROM `address`; 
+SELECT * FROM `cityinfo`;
+SELECT * FROM `compoany`;
+SELECT * FROM `hobby`;
+SELECT * FROM `infoentity`;
+SELECT * FROM `person`;
+SELECT * FROM `person_hobby`;
+SELECT * FROM `phone`;
 
 SELECT city FROM cityinfo WHERE zip = 8220; 
--- SELECT `city` FROM `cityinfo` WHERE `zip` = 2100; 
--- SELECT `city` FROM `cityinfo` WHERE `zip` = 3330; 
--- SELECT `city` FROM `cityinfo` WHERE `zip` = 4684; 
--- SELECT `city` FROM `cityinfo` WHERE `zip` = 5700; 
--- SELECT `city` FROM `cityinfo` WHERE `zip` = 6600;
--- SELECT `city` FROM `cityinfo` WHERE `zip` = 8220;
+SELECT `city` FROM `cityinfo` WHERE `zip` = 2100; 
+SELECT `city` FROM `cityinfo` WHERE `zip` = 3330; 
+SELECT `city` FROM `cityinfo` WHERE `zip` = 4684; 
+SELECT `city` FROM `cityinfo` WHERE `zip` = 5700; 
+SELECT `city` FROM `cityinfo` WHERE `zip` = 6600;
+SELECT `city` FROM `cityinfo` WHERE `zip` = 8220;
+
