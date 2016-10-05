44ff83b48e6a629059bec8c602e473c9f690e3e8
nickl <nickl@10.50.131.71>
2016-10-05 11:36:39 +0200

    minor changes in JSONConverter.class, CompanyJPA.class, CompanyFacade.class and CompanyRest!

--- src/main/java/JPA/CompanyJPA.java
+++ src/main/java/JPA/CompanyJPA.java
@@ -1,6 +1,9 @@
 package JPA;
 
 import RESTfacade.CompanyFacade;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import objects.Company;
 
 /**
@@ -9,21 +12,89 @@ import objects.Company;
  */
 public class CompanyJPA implements CompanyFacade {
 
-    @Override
-    public Company getCompany(String type, int typeNumber) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    EntityManagerFactory emf;
+
+    public CompanyJPA() {
+        emf = Persistence.createEntityManagerFactory("CA2");
     }
 
     @Override
-    public Company deleteCompany(int cvr) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Company getCompany(int cvr) {
+        EntityManager em = emf.createEntityManager();
+        Company c = null;
+        try {
+
+            em.getTransaction().begin();
+            c = em.find(Company.class, cvr);
+            em.getTransaction().commit();
+            return c;
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return null;
+        } finally{
+            em.close();
+        }
     }
 
     @Override
-    public Company editCompany(int cvr) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean deleteCompany(int cvr) {
+        EntityManager em = emf.createEntityManager();
+        try{
+            Company c = em.find(Company.class, cvr);
+            em.getTransaction().begin();
+            em.remove(c);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e){
+            em.getTransaction().rollback();
+            //Return exception!
+            return false;
+        } finally {
+            em.close();
+        }
     }
 
-
+    @Override
+    public boolean editCompany(Company comp) {
+        EntityManager em = emf.createEntityManager();
+        try{
+            Company c = em.find(Company.class, comp.getCvr());
+            em.getTransaction().begin();
+            c.setCvr(comp.getCvr());
+            c.setDescription(comp.getDescription());
+            c.setMarketValue(comp.getMarketValue());
+            c.setName(comp.getName());
+            c.setNumEmployees(comp.getNumEmployees());
+            em.getTransaction().commit();
+            
+            return true;
+        } catch(Exception e){
+            em.getTransaction().rollback();
+            //return exception !!!
+            return false;
+        } finally {
+            em.close();
+        }
+    }
+    
+    @Override
+    public boolean createCompany(Company comp){
+        EntityManager em = emf.createEntityManager();
+        try{
+            
+            em.getTransaction().begin();
+            em.persist(comp);
+            em.getTransaction().commit();
+            
+            return true;
+        } catch(Exception e){
+            em.getTransaction().rollback();
+            return false;
+        } finally {
+            em.close();
+        }
+        
+    }
 
 }
